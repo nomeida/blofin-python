103fec41b19b4330dd3fbbda3d3d47a5c06a6dce
Nomeida <nomeida@proton.me>
2024-08-07 20:36:58 +0200

    Auto update version for __init__.py

--- src/blofin/__init__.py
+++ src/blofin/__init__.py
@@ -1,1 +1,9 @@
-__version__ = "0.3.3"
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("blofin")
+except PackageNotFoundError:
+    # package is not installed
+    __version__ = "unknown"
+
+# Rest of your __init__.py content
